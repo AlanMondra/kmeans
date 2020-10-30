8dfcaaff7cd5837aa828debefb212f0d3f267807
AlanMondra <a01734565@itesm.mx>
2020-10-30 20:17:21 +0000

    Tarea4.py terminada

--- Tarea4.py
+++ Tarea4.py
@@ -10,14 +10,42 @@ from kneed import KneeLocator
 
 df = pd.read_csv('ulabox.csv')
 del df['order']
+dfp = df[["weekday", "total_items"]]
+# %%
+sdd=[]
+ks = range(1,14)
+for k in range(1,14):
+    km = KMeans(n_clusters=k)
+    km = km.fit(dfp)
+    sdd.append(km.inertia_)
+kneedle = KneeLocator(ks, sdd,S=1.0,curve="convex",direction="decreasing")
+kneedle.plot_knee()
+plt.show()
+k= round(kneedle.knee)
 
 # %%
-dfp = df[["weekday", "total_items"]]
-kmeans = KMeans(n_clusters=3).fit(df[["weekday", "total_items"]])
-sns.scatterplot(data=df, x="weekday", y="total_items", hue=kmeans.labels_)
+#for i in range(k):
+    #print("\nCLUSTER ",i)
+cluster0 = df[kmeans.labels_ == 0]
+cluster0.describe()
+
+# %%
+cluster1 = df[kmeans.labels_ == 1]
+cluster1.describe()
+# %%
+cluster2 = df[kmeans.labels_ == 2]
+cluster2.describe()
+# %%
+cluster3 = df[kmeans.labels_ == 3]
+cluster3.describe()
+# %%
+dfpi = df[["weekday", "Pets%"]]
+kmeans = KMeans(n_clusters=k).fit(dfpi)
+sns.scatterplot(data=df, x="weekday", y="Pets%", hue=kmeans.labels_)
 plt.show()
 """
 sns.heatmap(df.corr(),annot=True)
 items = df[df["total_items"] < 29]
 sns.boxplot(data=items,x="weekday",y="total_items")
 """
+# %%
